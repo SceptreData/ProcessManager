dcd72534c2b7db34f8c453d497d9901ba48048fd
David Bergeron <david.f.bergeron@gmail.com>
2017-02-17 23:46:53 -0700

    Added some minor documentation

--- processManager.h
+++ processManager.h
@@ -1,5 +1,22 @@
 #ifndef PROCESSMANAGER_H
 #define PROCESSMANAGER_H
+/*
+ *   ProcessManager v.01
+ *   David Bergeron
+ *
+ *   A system I implemented to handle various subsystems and calls inside a
+ *   game engine. Processes are updated everytime we call UpdateProcessManager().
+ *
+ *   Every process may have a child process, which it adds to our process list when
+ *   the parent process is complete. This allows us to chain various complex interactions,
+ *   for instance:
+ *       Process A -> Character paths towards destination.
+ *       Process B -> Character Interacts with object at destination.
+ *
+ *   If Process B is a child-process of Process A, it will only be added to the 
+ *   chain once Process A has succesfully completed.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -33,6 +50,7 @@ typedef struct processManager_t {
 
 ProcessManager *NewProcessManager(void);
 
+/* Here we register a processes Callback functions, and can pass data to udata */
 process_t *NewProcess (void (*onInit)(),
                        void (*onUpdate),
                        void (*onComplete),
@@ -41,12 +59,13 @@ process_t *NewProcess (void (*onInit)(),
                        void *udata
                       );
 
+/* Attach a process to our ProcessManager's process chain. */
 void AttachProcess(ProcessManager *manager, process_t *newProcess);
 
+/* Traverse our process chain, executing the appropriate callbacks. */
 void UpdateProcessManager(ProcessManager *manager, unsigned dt);
 
 bool ProcessIsDead(process_t *p);
-
 bool ProcessIsAlive(process_t *p);
 
 #endif // PROCESSMANAGER_H
